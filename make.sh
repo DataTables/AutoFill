d1cba54d43fa39084667820701c2f65e4d6820e0
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-06-18 15:51:52 +0100

    Complete rewrite of AutoFill to bring it up-to-date with the rest of DataTables for features, style and compatibility.
    The same basic features are still present - it performs an Excel like
    auto fill using a click and drag handle in the table cells, but how it
    actually operates is entirely different. The key change is that how data
    is filled is determined by plug-ins - the end user can select what fill
    type they want as each plug-in will determine if it can be used for a
    fill. This greatly increases the flexibiltiy of AutoFill as it can now
    be easily customised.
    
    Also full integration with Editor.

--- make.sh
+++ make.sh
@@ -12,18 +12,16 @@ DT_BUILT="${DT_SRC}/built/DataTables"
 
 # Copy CSS
 rsync -r css $OUT_DIR
-css_compress $OUT_DIR/css/dataTables.autoFill.css
-
-# Copy Images
-rsync -r images $OUT_DIR
+css_frameworks autoFill $OUT_DIR/css
 
 # Copy JS
 rsync -r js $OUT_DIR
 js_compress $OUT_DIR/js/dataTables.autoFill.js
+js_frameworks autoFill $OUT_DIR/js
 
 # Copy and build examples
 rsync -r examples $OUT_DIR
-examples_process $OUT_DIR
+examples_process $OUT_DIR/examples
 
 # Readme and license
 cp Readme.md $OUT_DIR

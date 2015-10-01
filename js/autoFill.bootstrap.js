8429a060e27b7ecf2d40e370b61758028f44f9f5
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-10-01 14:31:40 +0100

    New: Styling integration Javascript files now loadable with RequireJS or CommonJS New: When CommonJS loading AutoFill you can pass in a jQuery and or DataTables object Dev: Rejigging the structure of the UMD for the main file

--- js/autoFill.bootstrap.js
+++ js/autoFill.bootstrap.js
@@ -1,2 +1,25 @@
 
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables'], factory );
+	}
+	else if ( typeof exports === 'object' ) {
+		// Node / CommonJS
+		module.exports = function ($, dt) {
+			if ( ! $ ) { $ = require('jquery'); }
+			factory( $, dt || $.fn.dataTable || require('datatables') );
+		};
+	}
+	else if ( jQuery ) {
+		// Browser standard
+		factory( jQuery, jQuery.fn.dataTable );
+	}
+}(function( $, DataTable ) {
+'use strict';
+
+
 $.fn.dataTable.AutoFill.classes.btn = 'btn btn-primary';
+
+
+}));
--- js/dataTables.autoFill.js
+++ js/dataTables.autoFill.js
@@ -20,11 +20,25 @@
  *
  * For details please refer to: http://www.datatables.net
  */
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables'], factory );
+	}
+	else if ( typeof exports === 'object' ) {
+		// Node / CommonJS
+		module.exports = function ($, dt) {
+			if ( ! $ ) { $ = require('jquery'); }
+			factory( $, dt || $.fn.dataTable || require('datatables') );
+		};
+	}
+	else if ( jQuery ) {
+		// Browser standard
+		factory( jQuery, jQuery.fn.dataTable );
+	}
+}(function( $, DataTable ) {
+'use strict';
 
-(function( window, document, undefined ) {
-
-var factory = function( $, DataTable ) {
-"use strict";
 
 var _instance = 0;
 
@@ -938,22 +952,4 @@ DataTable.AutoFill = AutoFill;
 
 
 return AutoFill;
-};
-
-
-// Define as an AMD module if possible
-if ( typeof define === 'function' && define.amd ) {
-	define( ['jquery', 'datatables'], factory );
-}
-else if ( typeof exports === 'object' ) {
-    // Node/CommonJS
-    factory( require('jquery'), require('datatables') );
-}
-else if ( jQuery && !jQuery.fn.dataTable.AutoFill ) {
-	// Otherwise simply initialise as normal, stopping multiple evaluation
-	factory( jQuery, jQuery.fn.dataTable );
-}
-
-
-}(window, document));
-
+}));

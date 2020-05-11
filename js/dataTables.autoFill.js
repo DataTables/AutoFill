1a8164da08bda1d13638865523524509919c9ed2
Allan Jardine <allan@datatables.net>
2020-05-11 12:40:51 +0000

    Release: 2.3.5

--- js/dataTables.autoFill.js
+++ js/dataTables.autoFill.js
@@ -1,15 +1,15 @@
-/*! AutoFill 2.3.5-dev
- * ©2008-2019 SpryMedia Ltd - datatables.net/license
+/*! AutoFill 2.3.5
+ * ©2008-2020 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     AutoFill
  * @description Add Excel like click and drag auto-fill options to DataTables
- * @version     2.3.5-dev
+ * @version     2.3.5
  * @file        dataTables.autoFill.js
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     www.sprymedia.co.uk/contact
- * @copyright   Copyright 2010-2019 SpryMedia Ltd.
+ * @copyright   Copyright 2010-2020 SpryMedia Ltd.
  *
  * This source file is free software, available under the following license:
  *   MIT license - http://datatables.net/license/mit
@@ -1102,7 +1102,7 @@ AutoFill.actions = {
  * @static
  * @type      String
  */
-AutoFill.version = '2.3.5-dev';
+AutoFill.version = '2.3.5';
 
 
 /**

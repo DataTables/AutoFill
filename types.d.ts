22b70b7c476a358613f96e2ae8484ea339d4dcae
Allan Jardine <allan@datatables.net>
2022-10-21 10:28:42 +0000

    New: Rewrite of Typescript defination file to correctly extend DataTables core

--- types.d.ts
+++ types.d.ts
@@ -1,66 +1,76 @@
-// Type definitions for datatables.net-autofill 2.2
-// Project: https://datatables.net
-// Definitions by: Andy Ma <https://github.com/andy-maca>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.4
+// Type definitions for DataTables AutoFill
+//
+// Project: https://datatables.net/extensions/autofill/, https://datatables.net
+// Definitions by:
+//   SpryMedia
+//   Andy Ma <https://github.com/andy-maca>
 
 /// <reference types="jquery" />
-/// <reference types="datatables.net"/>
 
-declare namespace DataTables {
-  interface Settings {
-    /**
-     * autoFill extension options
-     */
-    autoFill?: boolean | AutoFillSettings;
-  }
+import DataTables, {Api} from 'datatables.net';
 
-  interface Api {
-    /**
-     * Initialise AutoFill on the DataTable
-     * 
-     * @returns Api for chaining with the additional autoFill methods
-     */
-    autoFill(): AutoFillFunctions | Api;
-  }
+export default DataTables;
 
-  interface AutoFillFunctions {
-    /**
-     * Disable AutoFill. Please note that this disallows future interactions with the table (until re-enabled).
-     * 
-     * @returns DataTables Api instance
-     */
-    disable(): Api;
+declare module 'datatables.net' {
+	interface Config {
+		/**
+		 * autoFill extension options
+		 */
+		autoFill?: boolean | ConfigAutoFill;
+	}
 
-    /**
-     * Enable end user and API modification of the focused cells in the DataTable. Differing levels of enablement are available via the optional parameter.
-     * 
-     * @param flag can be true or false to signify whether to enable or disable
-     * @returns DataTables Api instance
-     */
-    enable(flag?: string | boolean): Api;
+	interface Api<T> {
+		/**
+		 * Initialise AutoFill on the DataTable
+		 * 
+		 * @returns Api for chaining with the additional autoFill methods
+		 */
+		autoFill: ApiAutoFill<T>;
+	}
+}
 
-    /**
-     * This method will return a boolean value indicating if AutoFill is enabled or not on the selected table.
-     * 
-     * @returns boolean signifying if autofill is enables
-     */
-    enabled(): boolean;
-  }
+interface ConfigAutoFill {
+	/**
+	 * Always ask the end user if an action should be taken or not
+	 */
+	alwaysAsk?: boolean;
 
-  interface AutoFillSettings {
-    /**
-     * Always ask the end user if an action should be taken or not
-     */
-    alwaysAsk?: boolean;
-    /**
-     * Select the columns that can be auto filled
-     */
-    columns: string | number[];
-    /**
-     *
-     * Initial enablement state of AutoFill
-     */
-    enable?: boolean;
-  }
+	/**
+	 * Select the columns that can be auto filled
+	 */
+	columns?: string | number[];
+
+	/**
+	 *
+	 * Initial enablement state of AutoFill
+	 */
+	enable?: boolean;
+}
+
+interface ApiAutoFill<T> {
+	(): ApiAutoFillMethods<T>;
+}
+
+interface ApiAutoFillMethods<T> extends Api<T> {
+	/**
+	 * Disable AutoFill. Please note that this disallows future interactions with the table (until re-enabled).
+	 * 
+	 * @returns DataTables Api instance
+	 */
+	disable(): Api<T>;
+
+	/**
+	 * Enable end user and API modification of the focused cells in the DataTable. Differing levels of enablement are available via the optional parameter.
+	 * 
+	 * @param flag can be true or false to signify whether to enable or disable
+	 * @returns DataTables Api instance
+	 */
+	enable(flag?: string | boolean): Api<T>;
+
+	/**
+	 * This method will return a boolean value indicating if AutoFill is enabled or not on the selected table.
+	 * 
+	 * @returns boolean signifying if autofill is enables
+	 */
+	enabled(): boolean;
 }

aeb13f49415663a4a20bbdff3ecd09fcb3cb6eaa
Allan Jardine <allan@datatables.net>
2026-09-22 10:08:34 +0000

    Release 3.1.1

--- js/AutoFill.ts
+++ js/AutoFill.ts
@@ -164,7 +164,7 @@ export default class AutoFill {
 	};
 
 	/** AutoFill version */
-	static version = '3.1.0';
+	static version = '3.1.1';
 
 	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	 * Public methods (exposed via the DataTables API below)
